5b6210ed6920870553eb01c4e1d2c8ba0f7cc282
Irvin Feliciano <17111923@cdjuarez.tecnm.mx>
2020-11-14 20:22:58 -0700

    Protocolo HTTP (IAFS).

---
+++ ProtocoloHTTP/script.js
@@ -0,0 +1,136 @@
+
+let Foto1 = "Icono1";
+
+function CambioImagen1(){
+
+	let Img1 = document.getElementById("F1");
+
+	if (Foto1 == "Icono1"){
+		Img1.src = "Imagenes/Icono2.png";
+		Foto1 = "Icono2";
+	}
+	else {
+		Img1.src = "Imagenes/Icono1.png";
+		Foto1 = "Icono1";
+	} 
+
+}
+
+let Foto2 = "Icono2";
+
+function CambioImagen2(){
+
+	let Img2 = document.getElementById("F2");
+
+	if (Foto2 == "Icono2"){
+		Img2.src = "Imagenes/Icono1.png";
+		Foto2 = "Icono1";
+	}
+	else {
+		Img2.src = "Imagenes/Icono2.png";
+		Foto2 = "Icono2";
+	}
+
+}
+
+let Foto3 = "Icono1";
+
+function CambioImagen3(){
+
+	let Img3 = document.getElementById("F3");
+
+	if (Foto3 == "Icono1"){
+		Img3.src = "Imagenes/Icono2.png";
+		Foto3 = "Icono2";
+	}
+	else {
+		Img3.src = "Imagenes/Icono1.png";
+		Foto3 = "Icono1";
+	} 
+
+}
+
+let Foto4 = "Icono2";
+
+function CambioImagen4(){
+
+	let Img4 = document.getElementById("F4");
+
+	if (Foto4 == "Icono2"){
+		Img4.src = "Imagenes/Icono1.png";
+		Foto4 = "Icono1";
+	}
+	else {
+		Img4.src = "Imagenes/Icono2.png";
+		Foto4 = "Icono2";
+	}
+
+}
+
+let Foto5 = "Icono1";
+
+function CambioImagen5(){
+
+	let Img5 = document.getElementById("F5");
+
+	if (Foto5 == "Icono1"){
+		Img5.src = "Imagenes/Icono2.png";
+		Foto5 = "Icono2";
+	}
+	else {
+		Img5.src = "Imagenes/Icono1.png";
+		Foto5 = "Icono1";
+	} 
+
+}
+
+let Foto6 = "Icono2";
+
+function CambioImagen6(){
+
+	let Img6 = document.getElementById("F6");
+
+	if (Foto6 == "Icono2"){
+		Img6.src = "Imagenes/Icono1.png";
+		Foto6 = "Icono1";
+	}
+	else {
+		Img6.src = "Imagenes/Icono2.png";
+		Foto6 = "Icono2";
+	}
+
+}
+
+let Foto7 = "Icono1";
+
+function CambioImagen7(){
+
+	let Img7 = document.getElementById("F7");
+
+	if (Foto7 == "Icono1"){
+		Img7.src = "Imagenes/Icono2.png";
+		Foto7 = "Icono2";
+	}
+	else {
+		Img7.src = "Imagenes/Icono1.png";
+		Foto7 = "Icono1";
+	} 
+
+}
+
+let Foto8 = "Icono2";
+
+function CambioImagen8(){
+
+	let Img8 = document.getElementById("F8");
+
+	if (Foto8 == "Icono2"){
+		Img8.src = "Imagenes/Icono1.png";
+		Foto8 = "Icono1";
+	}
+	else {
+		Img8.src = "Imagenes/Icono2.png";
+		Foto8 = "Icono2";
+	}
+
+}
